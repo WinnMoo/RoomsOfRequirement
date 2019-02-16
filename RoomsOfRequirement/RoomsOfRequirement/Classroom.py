95418fecaddca0d681dc7f656cdc1cb5a6098e67
BornIncompetence <cagbrothers@gmail.com>
2019-02-16 00:50:22 -0800

    Cleaned up SubjectScraper.py

--- RoomsOfRequirement/RoomsOfRequirement/Classroom.py
+++ RoomsOfRequirement/RoomsOfRequirement/Classroom.py
@@ -5,33 +5,58 @@ Created on Tue Feb 12 16:56:23 2019
 
 @author: winn
 """
+from datetime import datetime
 
-class Classroom():
-    def __init__(self, classroomNumber, day, time):
-        self.classroomNumber = classroomNumber
-        self.day = day
-        
-        
-    def formatTime(time):
+
+class Classroom:
+    DOW = {'M': 'Monday', 'Tu': 'Tuesday',
+           'W': 'Wednesday', 'Th': 'Thursday', 'F': 'Friday'}
+
+    def __init__(self, day, time, classroomNumber):
+        self.formatClassRoomNumber(classroomNumber)
+        self.formatDays(day)
+        self.formatTimes(time)
+
+    def __str__(self):
+        return 'Days: {0:23} Time: {1:15} Building: {2} Room #: {3}'.format(
+            str(self.days),
+            self.start + '-' + self.end,
+            self.building,
+            self.room)
+
+    def formatClassRoomNumber(self, classroomNumber):
+        tokens = classroomNumber.split('-')
+        self.building = tokens[0]
+        self.room = tokens[1]
+
+    # If you guys want, you can change this to hold just the initials instead 
+    # of the full names of the dates
+    def formatDays(self, day):
+        self.days = []
+        for c in day:
+            if c == 'M' or c == 'W' or c == 'F':
+                self.days.append(self.DOW[c])
+                # self.days.append(c)
+            elif c == 'h' or c == 'u':
+                self.days.append(self.DOW['T' + c])
+                # self.days.append('T' + c)
+
+    def formatTimes(self, time):
         times = time.split('-')
-        startTime = times[0]
-        endTime = times[1]
-        
-        #TODO convert from 12hr format to 24hr format
-        # datetime.strptime(startTime, "%H:%M")
-        if endTime[-2:] == "AM":
-            startTime = startTime + 'AM'
+        self.start = times[0]
+        self.end = times[1]
+
+        # TODO convert from 12hr format to 24hr format
+        if ':' not in self.start:
+            self.start += ':00'
+        if ':' not in self.end:
+            # Removes the AM/PM from end of string, then adds it after ':00'
+            am_pm = self.end[-2:]
+            self.end = self.end[:-2] + ':00 ' + am_pm
+
+        if self.end[-2:] == 'AM':
+            self.start += 'AM'
         else:
-            startTime = endTime + 'PM'
+            self.start += 'PM'
         
-        
-        if ':' not in startTime: 
-            startTime = startTime + ':00'
-        
-        if ':' not in endTime:
-            endTime = endTime + ':00'
-            
-            
-        
-        self.StartTime
-        self.EndTime
+        # a = datetime.strptime(self.start, "%H:%M")
--- RoomsOfRequirement/RoomsOfRequirement/SubjectScraper.py
+++ RoomsOfRequirement/RoomsOfRequirement/SubjectScraper.py
@@ -1,61 +1,30 @@
-#This class is used to scrape all the classes listed under one subject
-#Example URL to scrape: 
-#http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2019/By_Subject/ACCT.html
+# This class is used to scrape all the classes listed under one subject
+# Example URL to scrape:
+# http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2019/By_Subject/ACCT.html
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from Classroom import Classroom
 
 
-data = [] #List used to hold all the data scraped from the html
+data = []  # List used to hold all the data scraped from the html
 pageToParse = urlopen(
     'http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2019/By_Subject/CECS.html')
 
-parsedPage = BeautifulSoup(pageToParse, "lxml")
-for element in parsedPage.find_all('td'): #gets every element that has the 'td' tag
-    data.append(element.text)  #add that element to the data list
+parsedPage = BeautifulSoup(pageToParse, "lxml")  # python3 -m pip install lxml
+# gets every element that has the 'td' tag
+for element in parsedPage.find_all('td'):
+    data.append(element.text)  # add that element to the data list
     element.next_sibling
 
-#lists used to hold the individual data sets for each class
-days = []
-times = []
 classrooms = []
-
-
-#data[] has all the info per class, every 11 lines is a new class
-dayIndex = 5 #every interval of 5+(11*n) is the day table column
-while dayIndex < len(data):
-    days.append(data[dayIndex]) #add the day to the days list
-    dayIndex = dayIndex + 11
-    
-timeIndex = 6
-while timeIndex < len(data):
-    times.append(data[timeIndex])
-    timeIndex = timeIndex + 11
-
-classIndex = 8
-while classIndex < len(data):
-    classrooms.append(data[classIndex])
-    classIndex = classIndex + 11
-
-
-#this block is used to remove unnecessary data such as classes with 'TBA' or 'Online-Only'
-i = len(classrooms)-1
-while i > 0:
-    if len(classrooms[i]) > 8:
-        del classrooms[i]
-        del times[i]
-        del days[i]
-    elif classrooms[i] == "TBA":
-        del classrooms[i]
-        del times[i]
-        del days[i]
-    i = i - 1
-    
-print(days)
-print(times)
-print(classrooms)
-
-
-
-
-
+i = 5  # The first entry is at 5, there is a new class every 11 entries
+while i < len(data):
+    fields = (data[i], data[i+1], data[i+3])  # Day, time, class respectively
+    # Classes labeled 'TBA' or 'Online-Only' are ignored
+    if len(fields[2]) > 8 or fields[2] == 'TBA':
+        pass
+    else:
+        classrooms.append(Classroom(fields[0], fields[1], fields[2]))
+        print(classrooms[-1])  # Can be removed later
+    i += 11
