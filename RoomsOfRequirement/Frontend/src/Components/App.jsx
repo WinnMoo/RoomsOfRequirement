266a9b6e011e2822d74902abe43eb0f33468dd78
BornIncompetence <cagbrothers@gmail.com>
2019-04-23 08:02:09 -0700

    Using HashRouter instead of BrowserRouter

--- RoomsOfRequirement/Frontend/src/Components/App.jsx
+++ RoomsOfRequirement/Frontend/src/Components/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './layout/Dashboard';
 import store from '../store';
@@ -11,12 +11,12 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <Router>
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route exact path="/:id" component={ClassDetails} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </Provider>
     );
   }
